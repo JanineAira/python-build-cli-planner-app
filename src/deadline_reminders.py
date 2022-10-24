7097248b784bd061840307e1a9e5d096b20a9a26
JanineDG <46197146+JanineAira@users.noreply.github.com>
2022-10-24 10:45:49 +0800

    Completed module

---
+++ src/deadline_reminders.py
@@ -0,0 +1,7 @@
+from abc import ABC, ABCMeta, abstractmethod
+from collections.abc import Iterable
+
+class DeadlineMetaReminder(Iterable, metaclass=ABCMeta):
+    @abstractmethod
+    def is_due(self):
+        pass
