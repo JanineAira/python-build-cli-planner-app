e5a3b5035eb77c99e98a5ad7d4e5d5d779a0e1d2
JanineDG <46197146+JanineAira@users.noreply.github.com>
2022-10-24 11:44:21 +0800

    Completed module

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from src.database import add_reminder, list_reminders
-from src.deadline_reminders import DateReminder, DeadlinedReminder
+from src.deadlined_reminders import DateReminder, DeadlinedReminder
 from src.external_reminders import EveningReminder
-from src.reminder import PoliteReminder
+# from src.reminder import PoliteReminder
 
-DeadlinedReminder.register(PoliteReminder)
+# DeadlinedReminder.register(PoliteReminder)
 
 def handle_input():
     choice = input("Choice: ")
